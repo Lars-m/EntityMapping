b08eb580b1b2c51bc390696769e53240a2d35816
lam <lam@cph-cnu1470h3l.efif.dk>
2014-09-25 14:08:03 +0200

    Initial Commit

---
+++ src/test/Tester.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @author Lars Mortensen
+ */
+public class Tester {
+  
+  public void test(){
+   
+  }
+  public static void main(String[] args) {
+   new Tester().test();
+  }
+}
